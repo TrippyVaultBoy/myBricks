3c58ba06b548e9da6cd0a857b9bc1f16770c5098
Tanner Saint <tsaint2004@gmail.com>
2025-08-18 06:40:04 +0000

    added swagger docs for /user

--- myBricks_API/routes/index.js
+++ myBricks_API/routes/index.js
@@ -13,6 +13,92 @@ router.get('/error', (req, res, next) => {
     throw new Error('test error.');
 });
 
+/**
+ * @swagger
+ * openapi: 3.0.0
+ * info:
+ *   title: MyBricks API
+ *   version: 1.0.0
+ * servers:
+ *   - url: http://localhost:4000
+ * /user:
+ *   post:
+ *     tags:
+ *       - default
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               email: user@example.com
+ *               username: user123
+ *               password: mypassword
+ *     responses:
+ *       '201':
+ *         description: Created
+ *         headers:
+ *           Content-Type:
+ *             schema:
+ *               type: string
+ *               example: application/json; charset=utf-8
+ *           Content-Length:
+ *             schema:
+ *               type: integer
+ *               example: 106
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *             example: |
+ *               {
+ *                   "message": "User user123 successfully created",
+ *                   "id": "68a2c7e07971b1d2f0126ab9",
+ *                   "email": "user@example.com"
+ *               }
+ *       '400':
+ *         description: Bad Request
+ *         headers:
+ *           Content-Type:
+ *             schema:
+ *               type: string
+ *               example: application/json; charset=utf-8
+ *           Content-Length:
+ *             schema:
+ *               type: integer
+ *               example: 31
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *             examples:
+ *               example-0:
+ *                 summary: User already exists
+ *                 value: |
+ *                   {
+ *                       "error": "User already exists"
+ *                   }
+ *               example-1:
+ *                 summary: Missing email
+ *                 value: |
+ *                   {
+ *                       "error": "Missing email"
+ *                   }
+ *               example-2:
+ *                 summary: Missing username
+ *                 value: |
+ *                   {
+ *                       "error": "Missing username"
+ *                   }
+ *               example-3:
+ *                 summary: Missing password
+ *                 value: |
+ *                   {
+ *                       "error": "Missing password"
+ *                   }
+ */
 router.post('/user', usersController.postNew);
 
 router.post('/login', authController.login);
