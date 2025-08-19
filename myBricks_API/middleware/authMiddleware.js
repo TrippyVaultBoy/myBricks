d1b1545c2fcfe90ffaf6be22ae2bb68b4804f1f5
Tanner Saint <tsaint2004@gmail.com>
2025-08-19 00:30:14 +0000

    fixed status code

--- myBricks_API/middleware/authMiddleware.js
+++ myBricks_API/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ function authMiddleware(req, res, next) {
     if (!token) return res.status(401).json({ error: 'Malformed token' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) return res.status(403).json({ error: 'Invalid or expired token' });
+        if (err) return res.status(401).json({ error: 'Invalid or expired token' });
 
         req.user = decoded;
         next();
