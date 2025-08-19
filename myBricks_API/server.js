17970a3b4218273775f0c747a728c4d7c5ddbd2e
Tanner Saint <tsaint2004@gmail.com>
2025-08-19 01:09:16 +0000

    added swagger documentation

--- myBricks_API/server.js
+++ myBricks_API/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const DBConnect = require('./utils/db.js');
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpecs = require('./utils/swagger.js');
 const requestLogger = require('./middleware/logger.js');
 const errorHandler = require('./middleware/errorHandler.js');
 const routes = require('./routes/index.js');
 require('dotenv').config();
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const server = express();
 server.use(express.json());
 server.use(requestLogger);
 server.use('/', routes);
 server.use(errorHandler);
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 DBConnect();
 
