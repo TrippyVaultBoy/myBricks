68e442ca0083b294b80058cc8bbc58ee8ff47ddc
Tanner Saint <tsaint2004@gmail.com>
2025-10-03 01:07:20 +0000

    added set search to home page

--- myBricks_frontend/src/components/HomePage.jsx
+++ myBricks_frontend/src/components/HomePage.jsx
@@ -3,8 +3,18 @@ import Footer from "./Footer";
 
 function HomePage() {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col  min-h-screen">
             <Header></Header>
+            <div className="mt-6 bg-[url(../assets/greeble_white.jpg)] mx-auto w-full max-w-screen-xl h-88">
+                <div className="flex flex-col gap-6 text-center items-center justify-center w-full h-full">
+                    <h2 className="text-3xl text-bricksTeal bg-bricksNavy rounded-2xl px-4 py-2">Find LEGO Sets</h2>
+                    <form className="flex gap-2">
+                        <label for="username" className="hidden">Find LEGO Sets</label>
+                        <input type="text" className="bg-bricksWhite text-center border-2 border-bricksNavy rounded-lg w-100 h-16" id="username" name="username" placeholder="Enter a LEGO set number"/>
+                    <button type="submit" className="bg-bricksRed text-bricksWhite rounded-lg p-4">Submit</button>
+                    </form>
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     )
